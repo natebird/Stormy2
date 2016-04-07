64d412bad5eb1a58e667bc270c4226c107ed7706
Nate Bird <natebird@gmail.com>
2016-04-07 15:02:39 -0400

    Move all the stuff from ViewController to WeeklyTableViewController

--- Stormy/WeeklyTableViewController.swift
+++ Stormy/WeeklyTableViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class WeeklyTableViewController: UITableViewController {
 
+  @IBOutlet weak var currentWeatherIcon: UIImageView!
+  @IBOutlet weak var currentTemperatureLabel: UILabel!
+  @IBOutlet weak var currentPrecipitationLabel: UILabel!
+  @IBOutlet weak var currentTemperatureRangeLabel: UILabel!
+  
+  let coordinates: (lat: Double, lon: Double) = (34.227801,-84.5339087)
+  private let forecastAPIKey = Constant.forecastAPIKey
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Set table view's background view property
-    tableView.backgroundView = BackgroundView()
+    configureView()
+    retrieveWeatherForecast()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func configureView() {
+    // Set table view's background view property
+    tableView.backgroundView = BackgroundView()
+    
+    // Change the font and size of the nav bar text in code. Skipped because I did it in IB
+    //    let navBarAttributesDictionary: [String: AnyObject]? = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    //    navigationController?.navigationBar.titleTextAttributes = navBarAttributesDictionary
   }
 
   // MARK: - Table view data source
@@ -34,4 +51,32 @@ class WeeklyTableViewController: UITableViewController {
     return 0
   }
 
+  
+  // MARK: Fetching Weather
+  
+  func retrieveWeatherForecast() {
+    let forecastService = ForecastService(APIKey: forecastAPIKey)
+    forecastService.getForecast(coordinates.lat, lon: coordinates.lon) {
+      (let currently) in
+      
+      if let currentWeather = currently {
+        
+        dispatch_async(dispatch_get_main_queue()) {
+          
+          if let temperature = currentWeather.temperature {
+            self.currentTemperatureLabel?.text = "\(temperature)º"
+          }
+          
+          if let precipitation = currentWeather.precipProbability {
+            self.currentPrecipitationLabel?.text = "Rain: \(precipitation)%"
+          }
+          
+          if let icon = currentWeather.icon {
+            self.currentWeatherIcon?.image = icon
+          }
+        }
+        
+      }
+    }
+  }
 }
