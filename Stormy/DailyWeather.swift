7467b6fd185ded5f203f867bb453e62a3151da64
Nate Bird <natebird@gmail.com>
2016-04-07 15:54:02 -0400

    Refactor weather models to have a wrapper struct including current weather and daily weather forecasts.

---
+++ Stormy/DailyWeather.swift
@@ -0,0 +1,76 @@
+//
+//  DailyWeather.swift
+//  Stormy
+//
+//  Created by Nate Bird on 4/7/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DailyWeather {
+  
+  let maxTemperature: Int?
+  let minTemperature: Int?
+  let humidity: Int?
+  let precipChance: Int?
+  let summary: String?
+  var icon: UIImage? = UIImage(named: "default.png")
+  var largeIcon: UIImage? = UIImage(named: "default_large.png")
+  var sunriseTime: String?
+  var sunsetTime: String?
+  var day: String?
+  let dateFormatter = NSDateFormatter()
+  
+  init(dailyWeatherDict: [String: AnyObject]?) {
+    maxTemperature = dailyWeatherDict!["temperatureMax"] as? Int
+    minTemperature = dailyWeatherDict!["temperatureMin"] as? Int
+    if let humidityFloat = dailyWeatherDict!["humidity"] as? Double {
+      humidity = Int(humidityFloat * 100)
+    } else {
+      humidity = nil
+    }
+    if let precipChanceFloat = dailyWeatherDict!["precipProbability"] as? Double {
+      precipChance = Int(precipChanceFloat * 100)
+    } else {
+      precipChance = nil
+    }
+    summary = dailyWeatherDict!["summary"] as? String
+    if let iconString = dailyWeatherDict!["icon"] as? String,
+      let iconEnum = Icon(rawValue: iconString) {
+        (icon, largeIcon) = iconEnum.toImage()
+    }
+    if let sunriseDate = dailyWeatherDict!["sunriseTime"] as? Double {
+      sunriseTime = self.timeStringFromUnixTime(sunriseDate)
+    } else {
+      sunriseTime = nil
+    }
+    if let sunsetDate = dailyWeatherDict!["sunsetTime"] as? Double {
+      sunsetTime = self.timeStringFromUnixTime(sunsetDate)
+    } else {
+      sunsetTime = nil
+    }
+    if let time = dailyWeatherDict!["time"] as? Double {
+      day = dayStringFromTime(time)
+    }
+
+  }
+  
+  func timeStringFromUnixTime(unixTime: Double) -> String {
+    let date = NSDate(timeIntervalSince1970: unixTime)
+    
+    // returns date formatted as 12 hour time
+    dateFormatter.dateFormat = "hh:mm a"
+    return dateFormatter.stringFromDate(date)
+  }
+  
+  func dayStringFromTime(time: Double) -> String {
+    let date = NSDate(timeIntervalSince1970: time)
+    
+    dateFormatter.locale = NSLocale(localeIdentifier: NSLocale.currentLocale().localeIdentifier)
+    dateFormatter.dateFormat = "EEEE"
+    
+    return dateFormatter.stringFromDate(date)
+  }
+}
---
+++ Stormy/Forecast.swift
@@ -0,0 +1,27 @@
+//
+//  Forecast.swift
+//  Stormy
+//
+//  Created by Nate Bird on 4/7/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct Forecast {
+  var currentWeather: CurrentWeather?
+  var weeklyWeather: [DailyWeather] = []
+  
+  init(weatherDictionary: [String: AnyObject]?) {
+    if let currentWeatherDictionary = weatherDictionary?["currently"] as? [String: AnyObject] {
+      CurrentWeather(weatherDictionary: currentWeatherDictionary)
+    }
+    if let weeklyWeatherArray = weatherDictionary?["daily"]?["data"] as? [[String: AnyObject]] {
+      for dailyWeather in weeklyWeatherArray {
+        let daily = DailyWeather(dailyWeatherDict: dailyWeather)
+        weeklyWeather.append(daily)
+      }
+    }
+
+  }
+}
